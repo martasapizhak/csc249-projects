35eebb317406670687bdcfc6f4005222f85486e2
martasapizhak <marta.i.sapizhak@gmail.com>
2022-10-31 21:16:17 -0400

    Fix the favicon error

---
+++ project1/client.py
@@ -0,0 +1,36 @@
+import concurrent.futures
+import sys
+import socket
+
+
+def main(argv):
+    address = argv[1]
+    port = int(argv[2])
+    path = argv[3]
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((address, port))  # connect to the server
+    message = "GET /" + path + " HTTP/1.1"
+    client_socket.send(message.encode())  # send message
+    data = client_socket.recv(1024).decode()  # receive response
+    print(data)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
+
+# main
+# take in args from cl
+# call open client
+
+# in client()
+
+# open client socket
+# client connects to server
+# send get request to server
+# accept response
+# display server response
+# client takes in the server IP, address or host name, the port where server,
+# the path at which the requested object is stored at the server.
